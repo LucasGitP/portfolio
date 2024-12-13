b95f7899cb320f150b9f8a7b244ede6b43076105
LucasGitP <lucascirilloberardi@gmail.com>
2024-12-12 22:20:02 -0300

    feat(constants.js): create variables with my info and sections

---
+++ src/constants.js
@@ -0,0 +1,22 @@
+export const sections = {
+  knowledge: "Knowdledge",
+  experience: "Experience",
+  reference: "References",
+  education: "Education",
+  projects: 'My Projects'
+};
+
+export const info = [
+  { name: "phone", direction: "https://wa.me/5491165865633" },
+  { name: "email", direction: "mailto:lucascirilloberardi@gmail.com" },
+  {
+    name: "linkedin",
+    direction: "https://www.linkedin.com/in/lucas-cirillo-berardi-0a966630b/",
+  },
+  { name: "github", direction: "https://github.com/LucasGitP" },
+  {
+    name: "location",
+    direction:
+      "https://www.google.com/maps/place/Buenos+Aires/@-34.6156548,-58.5156996,12z/data=!3m1!4b1!4m6!3m5!1s0x95bcca3b4ef90cbd:0xa0b3812e88e88e87!8m2!3d-34.6036844!4d-58.3815591!16zL20vMDFseTVt?entry=ttu&g_ep=EgoyMDI0MTIxMC4wIKXMDSoASAFQAw%3D%3D",
+  },
+];
