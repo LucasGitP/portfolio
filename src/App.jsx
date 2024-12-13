d97b03d39c37c64a839d0aa77f2a99c7035fd77c
LucasGitP <lucascirilloberardi@gmail.com>
2024-12-12 22:24:36 -0300

    feat(App.jsx): add Navbar and Info to my app

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
-
-import './App.css'
-
+import "./App.css";
+import Info from "./components/Info/Info";
+import Navbar from "./components/Navbar/Navbar";
 function App() {
   return (
-    <></>
-  )
+    <>
+      <Navbar></Navbar>
+      <Info></Info>
+    </>
+  );
 }
 
-export default App
+export default App;
