70bda7a4acfc30150858d964dcc4b41643ed0883
LucasGitP <lucascirilloberardi@gmail.com>
2024-12-12 22:22:10 -0300

    feat(MyPhoto.jsx): add my photo to a component and add styles

---
+++ src/components/MyPhoto/MyPhoto.jsx
@@ -0,0 +1,13 @@
+import "./MyPhoto.css";
+
+function MyPhoto() {
+  return (
+    <>
+      <div className="my-photo">
+        <img className="img" src="/assets/me.png" alt="my-photo" />
+      </div>
+    </>
+  );
+}
+
+export default MyPhoto;
