d597d9c6ad1b5c5d7f7188e2243e38adfe60f143
LucasGitP <lucascirilloberardi@gmail.com>
2024-12-17 12:45:31 -0300

    feat(About.jsx): About section component

---
+++ src/components/About/About.jsx
@@ -0,0 +1,13 @@
+import Title from "../Title/Title";
+export default function About() {
+  return (
+    <div>
+      <Title>
+        <h2>About me</h2>
+      </Title>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa incidunt eveniet delectus, omnis eos blanditiis dolore facilis facere consectetur nihil voluptas? Quae omnis quasi architecto culpa. Voluptas a itaque ducimus.
+      Totam voluptatum minima omnis. Enim nostrum voluptate voluptatem ut officia a, cumque eos explicabo aliquam? Quam voluptates quos, dolores delectus, beatae nemo doloribus fugiat enim at ab voluptatibus voluptatum quibusdam?
+      Similique soluta repellat quasi distinctio? Aliquam laudantium nemo temporibus facere corrupti sunt officia? Quasi harum itaque voluptatibus voluptas dolorum voluptatem beatae dignissimos reiciendis commodi, repellat et aliquam similique reprehenderit quia?</p>
+    </div>
+  );
+}
