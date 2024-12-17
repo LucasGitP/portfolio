c6837c035545d6266fb6b2ee29bba4df388779c9
LucasGitP <lucascirilloberardi@gmail.com>
2024-12-17 12:43:48 -0300

    feat(Subject.jsx): component to create card subjects

---
+++ src/components/Subject/Subject.jsx
@@ -0,0 +1,11 @@
+import './Subject.css'
+export default function Subject({ title, children }) {
+  return (
+    <>
+      <div className="container-subject">
+        <h4>{title}</h4>
+        {children}
+      </div>
+    </>
+  );
+}
