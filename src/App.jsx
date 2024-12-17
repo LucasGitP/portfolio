cd8828bc223009918378c2d2f801d53b9a73cd24
LucasGitP <lucascirilloberardi@gmail.com>
2024-12-17 12:49:18 -0300

    feat(App.jsx): add About and Projects sections

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import "./App.css";
 import Info from "./components/Info/Info";
 import Navbar from "./components/Navbar/Navbar";
+import Projects from "./components/Projects/Projects";
+import About from "./components/About/About";
 function App() {
   return (
     <>
       <Navbar></Navbar>
       <Info></Info>
+      <About></About>
+      <Projects></Projects>
     </>
   );
 }
