9560228fa3a49f39549ff4cf94d52d810f908367
LucasGitP <lucascirilloberardi@gmail.com>
2024-12-18 18:20:57 -0300

    feat(Section.jsx): add an id

--- src/components/Section/Section.jsx
+++ src/components/Section/Section.jsx
@@ -4,7 +4,7 @@ import Title from "../Title/Title";
 export default function Section({ section, children }) {
   return (
     <>
-      <section className="section">
+      <section className="section" id={section}>
         {section && <Title section={section}></Title>}
         {children}
       </section>
