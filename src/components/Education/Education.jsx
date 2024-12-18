344273df5b96c1d30010143a34a5e7c839429424
LucasGitP <lucascirilloberardi@gmail.com>
2024-12-18 17:10:49 -0300

    feat(Education.jsx): create a section with all my education cards

---
+++ src/components/Education/Education.jsx
@@ -0,0 +1,15 @@
+import { education } from "../../constants.JS";
+import Section from "../Section/Section";
+import EducationCard from "./EducationCard";
+
+export default function Education() {
+  return (
+    <Section section="education">
+      <div className="education">
+        {education.map((e) => (
+          <EducationCard education={e} key={e.level}></EducationCard>
+        ))}
+      </div>
+    </Section>
+  );
+}
