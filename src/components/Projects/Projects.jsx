135ce7e5c37983863e5af05d416dd5a7bb7b7d70
LucasGitP <lucascirilloberardi@gmail.com>
2024-12-18 18:20:05 -0300

    fix(ProjectCard.jsx): [object,object] key error

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -11,7 +11,7 @@ export default function Projects() {
           {projects &&
             projects.map((project) => (
               <ProjectCard
-                key={project}
+                key={project.name}
                 project={project}
               ></ProjectCard>
             ))}
