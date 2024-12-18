a788f22c42ecbec1d2c4c3688c5855f6d6a123ed
LucasGitP <lucascirilloberardi@gmail.com>
2024-12-18 17:11:52 -0300

    feat(App.jsx): add Education and Expirience sections

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,17 @@ import Info from "./components/Info/Info";
 import Navbar from "./components/Navbar/Navbar";
 import Projects from "./components/Projects/Projects";
 import About from "./components/About/About";
+import Expirience from "./components/Expirience/Expirience";
+import Education from "./components/Education/Education";
+
 function App() {
   return (
     <>
       <Navbar></Navbar>
       <Info></Info>
       <About></About>
+      <Education></Education>
+      <Expirience></Expirience>
       <Projects></Projects>
     </>
   );
