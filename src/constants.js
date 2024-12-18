dfe9cb2372d9d335249abc8f485fc7b49074869b
LucasGitP <lucascirilloberardi@gmail.com>
2024-12-18 16:57:32 -0300

    feat(constants.js): add more projects and change info to contact

--- src/constants.js
+++ src/constants.js
@@ -6,7 +6,7 @@ export const sections = {
   projects: "Projects",
 };
 
-export const info = [
+export const contact = [
   { name: "phone", direction: "https://wa.me/5491165865633" },
   { name: "email", direction: "mailto:lucascirilloberardi@gmail.com" },
   {
@@ -25,7 +25,7 @@ export const projects = [
   {
     name: "Back office",
     img: "back-office.png",
-    tools: ["react", "vite", "sqlite3", "express"],
+    tools: ["react", "vite", "express", "sqlite3"],
     team: "GM2 Bootcamp",
     description: "",
     link: "https://github.com/gm2dev/bootcamp-2024-tp-final",
@@ -33,23 +33,66 @@ export const projects = [
     finalize: "29/11/2024",
   },
   {
-    name: "Back office",
-    img: "back-office.png",
-    tools: ["react", "vite", "sqlite3", "express"],
-    team: "GM2 Bootcamp",
+    name: "Pokedex",
+    img: "pokedex.png",
+    tools: ["javaScript", "html", "css"],
+    team: "Lucas Cirillo Berardi",
     description: "",
-    link: "https://github.com/gm2dev/bootcamp-2024-tp-final",
-    start: "13/11/2024",
-    finalize: "29/11/2024",
+    link: "https://github.com/LucasCirilloBerardi/pokedex",
+    start: "1/10/2024",
+    finalize: "7/10/2024",
   },
   {
-    name: "Back office",
-    img: "back-office.png",
-    tools: ["react", "vite", "sqlite3", "express"],
-    team: "GM2 Bootcamp",
+    name: "QuizGame",
+    img: "quiz.png",
+    tools: ["javaScript", "html", "css"],
+    team: "GM2dev Bootcamp",
     description: "",
-    link: "https://github.com/gm2dev/bootcamp-2024-tp-final",
-    start: "13/11/2024",
+    link: "https://github.com/LucasCirilloBerardi/groupProyect1",
+    start: "1/9/2024",
+    finalize: "15/9/2024",
+  },
+  {
+    name: "Tickets",
+    img: "tickets.png",
+    tools: ["html", "css"],
+    team: "Lucas Cirillo Berardi",
+    description: "",
+    link: "https://github.com/LucasCirilloBerardi/Gm2Repo",
+    start: "1/06/2024",
+    finalize: "2/06/2024",
+  },
+];
+
+export const experience = [
+  {
+    company: "GM2Dev",
+    start: "1/06/2024",
     finalize: "29/11/2024",
+    img: "gm2.svg",
+    area: "Web Development",
+    tools: ["react", "html", "css", "javaScript", "sqlite3", "express"],
+    description: "",
+  },
+];
+
+export const education = [
+  {
+    year: "2024",
+    level: "Web Development Bootcamp",
+    instution: "Gm2dev",
+    title: "Front-end Developer",
+  },
+  {
+    year: "2018-2024",
+    level: "Technical High School",
+    instution: "Ins. Ind. Luis A. Huergo",
+    title: "Computer Technician",
+  },
+  {
+    year: "2011-2018",
+    level: "Elementary Education",
+    instution: "Gm2dev",
+    title: "Instituto Santa Cruz",
   },
 ];
