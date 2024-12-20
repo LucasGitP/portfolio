ec2006a2c99da47151603d07697842fb9a16b0d8
LucasGitP <lucascirilloberardi@gmail.com>
2024-12-20 16:04:00 -0300

    feat(Navbar.jsx): create a burger button

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -5,9 +5,19 @@ import "./Navbar.css";
 export default function Navbar() {
   const links = Object.keys(sections);
   return (
-    <>
-      <nav className="navbar">
-        <ul className="list">
+    <nav className="navbar">
+      <div className="burger_menu">
+        <input
+          type="checkbox"
+          id="burger_toggle"
+          className="burger_menu_input"
+        />
+        <label htmlFor="burger_toggle" className="burger_menu_label">
+          <span className="burger_menu_span"></span>
+          <span className="burger_menu_span"></span>
+          <span className="burger_menu_span"></span>
+        </label>
+        <ul className="menu_list">
           {links.map((link) => (
             <li key={link}>
               <Link
@@ -15,13 +25,14 @@ export default function Navbar() {
                 smooth={true}
                 duration={800}
                 offset={-window.innerHeight / 4}
+                className="menu_list_links"
               >
                 {link.toUpperCase()}
               </Link>
             </li>
           ))}
         </ul>
-      </nav>
-    </>
+      </div>
+    </nav>
   );
 }
